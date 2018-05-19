ec308f67c0a5d9367e63dde8a728a43da65286d2
Seyed Hesam <seyedhesamsoleymani@gmail.com>
2018-05-19 18:14:55 +0430

    big change

---
+++ members/asal/js/index4.js
@@ -0,0 +1,68 @@
+
+document.addEventListener('DOMContentLoaded',function(event){
+
+  var dataText = [ "نوازندۀ دف،کونگا،درامز،کاخن", "فارغ التحصیل گریم تئاتر و سینما " , "دارای مدرک بین المللی Fashion Designer " ];
+
+
+
+  function typeWriter(text, i, fnCallback) {
+	 
+	  
+
+    if (i < (text.length)) {
+     
+     document.querySelector(".headline").innerHTML = text.substring(0, i+1) +'<span class="typewriter" aria-hidden="true"></span>';
+
+
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+
+    else if (typeof fnCallback == 'function') {
+
+      setTimeout(fnCallback, 400);
+    }
+  }
+
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+		
+    
+          StartTextAnimation(0);
+     
+     }
+
+    if (i < dataText[i].length) {
+   
+     typeWriter(dataText[i], 0, function(){
+		 
+		 function clearText() {
+			 setTimeout(function(){
+			      var foo = $(".headline").html().replace('<span class="typewriter" aria-hidden="true"></span>',"");
+				 if(foo.charAt(foo.length-1)===">") {
+				 				      var newX= foo.substring(0,foo.length-4);
+				 } else {
+			      var newX= foo.substring(0,foo.length-1);						
+						}
+				 $(".headline").html(newX+'<span class="typewriter" aria-hidden="true"></span>')
+				 if(newX.length>0) {				 
+					 clearText();
+				 } else {
+
+       				  StartTextAnimation(i + 1);
+				 }
+		  },50);
+		 }
+		  setTimeout(function() {
+          clearText();
+        }, 400);
+		 
+		
+     });
+    } else {
+	}
+  }
+
+  StartTextAnimation(0);
+});
